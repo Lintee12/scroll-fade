f1c526e6bdf9eddb57239e4b356f5d83b6e076ea
Evan <lintonevan@gmail.com>
2024-02-16 13:51:49 -0400

    update script

--- bin/js/scrollFade.js
+++ bin/js/scrollFade.js
@@ -1,20 +1,20 @@
 //1. put script at the bottom of the body tag.
 //2. add the "scroll-fade" class to any element you want to fade.
 const fadeTime = 1000; // time it takes for the element to become fully visible
-const fadeDelay = 25; // delay before next element can fade
-const fadeDistance = 1.25; // distance the element needs to be on screen to fade (window.innerHeight / revealDistance)
+const fadeDelay = 15; // delay before next element can fade
+const fadeDistance = 1.15; // distance the element needs to be on screen to fade (window.innerHeight / revealDistance)
 const ease = true; // ease in the fade
 
 let scrollFadeElements = [];
 
-window.onload = () => {
+document.addEventListener('DOMContentLoaded', () => {
     scrollFadeElements = document.querySelectorAll('.scroll-fade');
     scrollFadeElements.forEach(el => {
         el.style.opacity = 0;
         el.dataset.didFade = "false"
     })
     checkElements();
-}
+})
 
 document.addEventListener('scroll', () => {
     checkElements();
